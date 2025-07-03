ddee225dbd1b5901c0e513d282a354755199efa7
supa <sujithamar08@gmail.com>
2025-07-03 09:22:47 +0200

    brace removal

--- FD Rate Calculater/script.js
+++ FD Rate Calculater/script.js
@@ -7,19 +7,15 @@ function fdRate(){
     //3-6 months = 6.5%
     //7-9 months = 6.8%
     //>=10 months = 7.0%
-if (months < 3){
+if (months < 3)
         rate = 5.8;
-    }
-    else if(months >= 3 && months <= 6){
+    else if(months >= 3 && months <= 6)
         rate = 6.5;
-    }
-    else if(months >7 && months <= 9){
+    else if(months >7 && months <= 9)
         rate = 6.8;
-    }
-    else{
+    else
         rate = 7.0;
-    }
-  let result =document.querySelector("#result");
+let result =document.querySelector("#result");
     result.innerHTML = `The rate of interest for ${months} months is ${rate}%`;
     result.style.color = "green";
 }
